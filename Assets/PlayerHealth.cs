67ad5325e5c965ac2f42ff7910b2454e26935231
Lieanq <146747445+amogus212@users.noreply.github.com>
2025-06-15 17:54:52 +0300

    ScoreManager

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -7,22 +7,31 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 100;
     private int currentHealth;
     public GameObject gameOverUI;
+    public Text healthText;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        UpdateHealthUI();
         if (gameOverUI != null) gameOverUI.SetActive(false);
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        if(currentHealth <= 0)
+        UpdateHealthUI();
+        if (currentHealth <= 0)
         {
             Die();
         }
 
+    }
+
+    void UpdateHealthUI()
+    {
+        if (healthText != null)
+            healthText.text = "Health" + currentHealth;
     }
 
     void Die()
